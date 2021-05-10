cf2984676e5dabca33f9aa6ab8b2c712b6688436
John S <jdsdba@gmail.com>
2021-05-10 17:30:35 +0000

    shebang work

---
+++ no_shebang.py
@@ -0,0 +1,7 @@
+import getpass
+usr=getpass.getuser()
+
+print("ello!")
+print("Did you say, hello?")
+print(f"No, I said ello {usr}, but that\'s close enough.")
+
---
+++ py3_shebang.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import getpass
+usr=getpass.getuser()
+
+print("ello!")
+print("Did you say, hello?")
+print(f"No, I said ello {usr}, but that\'s close enough.")
+
