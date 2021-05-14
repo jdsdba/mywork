2de8e17fdcf152a8747e3fb480bd4a21e4c259af
John S <jdsdba@gmail.com>
2021-05-14 03:11:27 +0000

    python and apis

---
+++ catfacts/custom1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Alta3 Research | Author: RZFeeser@alta3.com"""
+
+# imports always go at the top of your code
+import requests
+import pprint
+
+def main():
+    """Run time code"""
+    ## create r, which is our request object
+    r = requests.get('https://cat-fact.herokuapp.com/facts')
+
+    ## catfact is our iterable -- that just means it will take on the values found within
+    ## r.json()["all"], one after the next-- which happens to be a dictionary
+    ## the code within the loop, says, "from that single dictionary
+    ## print the value associated with text"
+    pprint.pprint(r.text)
+main()
+
